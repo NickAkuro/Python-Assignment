68518eb2b9b2da7cc1ae2c5124aeaeef9372135c
NickAkuro <124090950+NickAkuro@users.noreply.github.com>
2025-08-11 05:56:42 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,23 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation(+, -, *, /): ")
+
+if operation == '+':
+    result = num1 + num2
+    print(f"{int(num1)} + {int(num2)} = {int(result)}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Cannot dive by zero.")
+else:
+    print("Error: Invalid operation. Please enter +, -, *, or /.")
+    
+    
